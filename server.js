895878c749d0de6feee5acad607c4dccb5776366
Nataliya <nataliya.d.nataliya@gmail.com>
2022-02-15 17:46:07 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({json: true}));
+
+const notes = [];
+
+let nextId = 1;
+
+const router = new Router();
+
+router.get('/', async (ctx) => {
+  ctx.response.body = 'server response';
+})
+
+router.get('/notes', async (ctx) => {
+  ctx.response.body = notes;
+});
+
+router.post('/notes', async(ctx) => {
+  notes.push({...ctx.request.body, id: nextId++});
+  ctx.response.status = 204;
+});
+
+router.delete('/notes/:id', async(ctx) => {
+  const noteId = Number(ctx.params.id);
+  const index = notes.findIndex(o => o.id === noteId);
+  if (index !== -1) {
+    notes.splice(index, 1);
+  }
+  ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback()).listen(+port);
